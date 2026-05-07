262ddf35dc448a292f62536b5414cd748db4b8f6
IsaacYng <yonghangissac@gmail.com>
2026-05-07 13:32:03 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+        // Auto-calculate Age
+        function calculateAge(birthDate) {
+            const today = new Date();
+            const birth = new Date(birthDate);
+            let age = today.getFullYear() - birth.getFullYear();
+            const monthDiff = today.getMonth() - birth.getMonth();
+            if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
+                age--;
+            }
+            return age;
+        }
+
+        document.getElementById('display-age').innerText = calculateAge('2005-10-26') + ' Years';
+
